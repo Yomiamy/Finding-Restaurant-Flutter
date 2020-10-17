5d4e25f56c67b3ef0fbfa7ebabca84bbf3190253
Randy <o1984531@gmail.com>
2020-10-17 18:17:23 +0800

    Add constants for color, font size and image name

---
+++ flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static String NO_IMAGE = "images/empty.png";
+}
---
+++ flutter_restaruant/flutter_restaruant/lib/utils/Dimens.dart
@@ -0,0 +1,8 @@
+class Dimens {
+  static const double xlFontSize = 10;
+  static const double lFontSize = 12;
+  static const double mFontSize = 14;
+  static const double hFontSize = 16;
+  static const double xhFontSize = 18;
+  static const double xxhFontSize = 20;
+}
