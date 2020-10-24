3b5c4c82b9b56d8ae0ee283ce727b315f98b5246
Randy <o1984531@gmail.com>
2020-10-24 20:01:43 +0800

    Define api and related constant paramenters

--- flutter_restaruant/flutter_restaruant/lib/api/APIClz.dart
+++ flutter_restaruant/flutter_restaruant/lib/api/APIClz.dart
@@ -1,17 +1,48 @@
 import 'package:flutter_restaruant/api/dio/DioClient.dart';
 import 'package:flutter_restaruant/model/ResultVo.dart';
+import 'package:flutter_restaruant/utils/Constants.dart';
 import 'package:logger/logger.dart';
 import 'package:retrofit/retrofit.dart';
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' hide Headers;
+import 'package:retrofit/http.dart';
 
 part 'APIClz.g.dart';
 
-@RestApi(baseUrl: "http://192.168.6.200:31199/")
+@RestApi(baseUrl: Constants.BASE_URL)
 abstract class APIClz {
   factory APIClz(Dio dio, {String baseUrl}) = _APIClz;
 
-  @POST("/v1/login")
-  Future<ResultVo> login(@Field() String account,@Field() String password);
+  @POST("/oauth2/token")
+  Future<ResultVo> fetchToken(
+      @Header("Content-Type") String contentType,
+      @Header("Authorization") String authorization,
+      @Field("grant_type") String grantType,
+      @Field("client_id") String clientId,
+      @Field("client_secret") String clientSecret);
+
+
+  @POST("/v3/businesses/search")
+  @Headers(<String, String>{"Content-Type": "application/json"})
+  Future<ResultVo> businessesSearch(@Header("Authorization") String token,
+      @Field("term") String term,
+      @Field("latitude") String latitude,
+      @Field("longitude") String longitude,
+      @Field("locale") String locale,
+      @Field("limit") String limit,
+      @Field("openAt") String openAt,
+      @Field("sortBy") String sortBy,
+      @Field("price") String price);
+
+  @POST("/v3/businesses")
+  @Headers(<String, String>{"Content-Type": "application/json"})
+  Future<ResultVo> business(@Header("Authorization") String token,
+      @Field("locale") String locale);
+
+  @POST("/v3/businesses/{id}/reviews")
+  @Headers(<String, String>{"Content-Type": "application/json"})
+  Future<ResultVo> review(@Header("Authorization") String token,
+      @Path() String id,
+      @Field("locale") String locale);
 }
 
 final dioClient = DioClient(
--- flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
+++ flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
@@ -1,8 +1,16 @@
 class Constants {
   /// [API]
-  static int CONNECTION_TIEMOUT = 30000;
-  static int RECEIVE_TIEMOUT = 30000;
+  static const OAUTH_GRANT_TYPYE = "client_credentials";
+  static const CLIENT_ID = "uy1lqpYWe0DVZaTM2wcsCw";
+  static const CLIENT_SECRET = "9KsAARABl3Rey3kM7anF9hHfCGzD802Rk7CVqUICQPwNLFrLnOPiRCXWdhKHGZhD";
+  static const AUTH_TOKEN = "4htbz9nLozJ_-Xw-13LpeVWrEIRWZt4IrgTOQXstx7M1DCVeUIoxIZkQ0XVLVEtD2Gy2Vp1FjA2WRz5DOpZMSxHfXVBLR3gi0DeMXIV3X1bCHYoMoJ-_TZLfBn0ZWnYx";
+
+  static const BASE_URL = "https://api.yelp.com";
+
+  static String TOKEN = "Bearer ";
+  static const int CONNECTION_TIEMOUT = 30000;
+  static const int RECEIVE_TIEMOUT = 30000;
 
   /// [UI]
-  static String NO_IMAGE = "images/empty.png";
+  static const String NO_IMAGE = "images/empty.png";
 }
