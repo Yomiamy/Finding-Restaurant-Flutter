bc2e992d2493cc902c6999e725718d0b5ea56dbd
Randy <o1984531@gmail.com>
2021-06-07 16:26:19 +0800

    Adjust the restaurant detail cell list

--- flutter_restaruant/flutter_restaruant/lib/flow/restaurant/RestaurantDetailPage.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/restaurant/RestaurantDetailPage.dart
@@ -2,15 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_restaruant/component/cell/restaurant_detail/RestaurantDetailCellCollection.dart';
 import 'package:flutter_restaruant/model/YelpRestaurantDetailInfo.dart';
-import 'package:flutter_restaruant/utils/Dimens.dart';
 import 'package:flutter_restaruant/utils/UIConstants.dart';
-import 'package:flutter_restaruant/utils/Constants.dart';
 
 class RestaurantDetailPage extends StatefulWidget {
-  static const int HEAD_IMAGE_H = 200;
-  static const int MAP_IMAGE_W = 140;
-  static const int MAP_IMAGE_H = 140;
 
   final YelpRestaurantDetailInfo detailInfo;
 
@@ -21,17 +17,6 @@ class RestaurantDetailPage extends StatefulWidget {
 }
 
 class RestaurantDetailState extends State<RestaurantDetailPage> {
-
-  Widget createBusinessTimeRow(String weedDay, String startTime, String endTime) =>
-      Padding(
-        padding: EdgeInsets.only(top: 5),
-        child: Stack(children: [
-          Align(child: Text(weedDay), alignment: Alignment.centerLeft),
-          Align(
-              child: Text("$startTime - $endTime"),
-              alignment: Alignment.centerRight)
-        ])
-      );
 
   @override
   Widget build(BuildContext context) => PlatformScaffold(
@@ -46,116 +31,14 @@ class RestaurantDetailState extends State<RestaurantDetailPage> {
           title: Text(widget.detailInfo.name ?? "",
               style: TextStyle(color: Colors.white)),
           backgroundColor: Color(UIConstants.AppBarColor)),
-      body: ListView(children: [
-        FadeInImage.assetNetwork(
-            placeholder: UIConstants.NO_IMAGE,
-            image: widget.detailInfo.image_url ?? "",
-            imageCacheHeight: RestaurantDetailPage.HEAD_IMAGE_H,
-            imageCacheWidth: MediaQuery.of(context).size.width.toInt(),
-            placeholderCacheHeight: RestaurantDetailPage.HEAD_IMAGE_H,
-            placeholderCacheWidth: MediaQuery.of(context).size.width.toInt(),
-            fit: BoxFit.fill,
-            width: MediaQuery.of(context).size.width,
-            height: RestaurantDetailPage.HEAD_IMAGE_H.toDouble()),
-        Padding(
-            padding: EdgeInsets.only(left: 5, right: 5, top: 10),
-            child: Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
-              SizedBox(
-                  width: RestaurantDetailPage.MAP_IMAGE_H.toDouble(),
-                  height: RestaurantDetailPage.MAP_IMAGE_W.toDouble(),
-                  child: FadeInImage.assetNetwork(
-                      placeholder: UIConstants.NO_IMAGE,
-                      imageErrorBuilder: (context, error, trace) =>
-                          Image.asset(UIConstants.NO_IMAGE),
-                      image: "https://staticmapmaker.com/img/google@2x.png",
-                      imageCacheHeight: RestaurantDetailPage.MAP_IMAGE_H,
-                      imageCacheWidth: RestaurantDetailPage.MAP_IMAGE_W,
-                      placeholderCacheHeight: RestaurantDetailPage.MAP_IMAGE_H,
-                      placeholderCacheWidth: RestaurantDetailPage.MAP_IMAGE_W,
-                      fit: BoxFit.fill)),
-              Expanded(
-                  child: Container(
-                      padding: EdgeInsets.only(left: 10),
-                      child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("235 新北市中和區中和路238號1111111",
-                                style: TextStyle(fontWeight: FontWeight.w700),
-                                overflow: TextOverflow.ellipsis),
-                            Row(children: <Widget>[
-                              Text("電話:",
-                                  style:
-                                      TextStyle(fontWeight: FontWeight.w700)),
-                              SizedBox(width: 10),
-                              Text("+8860982736457",
-                                  style: TextStyle(color: Colors.blue))
-                            ]),
-                            Text("餐廳", style: TextStyle(color: Colors.grey)),
-                            Image.asset("images/Star_rating_2_of_5.png",
-                                height: 20),
-                            Text("1則評論", style: TextStyle(color: Colors.grey)),
-                            DecoratedBox(
-                                decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(15.0))),
-                                child: Padding(
-                                    padding: EdgeInsets.all(3),
-                                    child: Text("close",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: Dimens.mFontSize))))
-                          ]))),
-            ])),
-        Container(
-            padding: EdgeInsets.only(top: 10),
-            height: MediaQuery.of(context).size.width / 3,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 10,
-                itemBuilder: (context, index) => Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 5),
-                      child: FadeInImage.assetNetwork(
-                          placeholder: UIConstants.NO_IMAGE,
-                          image: widget.detailInfo.image_url ?? "",
-                          imageCacheHeight: RestaurantDetailPage.HEAD_IMAGE_H,
-                          imageCacheWidth:
-                              MediaQuery.of(context).size.width.toInt(),
-                          placeholderCacheHeight:
-                              RestaurantDetailPage.HEAD_IMAGE_H,
-                          placeholderCacheWidth:
-                              MediaQuery.of(context).size.width.toInt(),
-                          fit: BoxFit.fill,
-                          width: MediaQuery.of(context).size.width / 3,
-                          height: MediaQuery.of(context).size.width / 3),
-                    ))),
-        Padding(
-            padding: EdgeInsets.only(top: 10),
-            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-              Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: DecoratedBox(
-                      decoration: BoxDecoration(color: Colors.grey),
-                      child: Center(
-                          child: Text("營業時間",
-                              style: TextStyle(
-                                  fontSize: Dimens.xhFontSize,
-                                  fontWeight: FontWeight.bold))))),
-              Padding(
-                padding: EdgeInsets.only(left: 10, right: 10),
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  this.createBusinessTimeRow("星期一", "1700", "2200"),
-                  this.createBusinessTimeRow("星期二", "1700", "2200"),
-                  this.createBusinessTimeRow("星期三", "1700", "2200"),
-                  this.createBusinessTimeRow("星期四", "1700", "2200"),
-                  this.createBusinessTimeRow("星期五", "1700", "2200"),
-                  this.createBusinessTimeRow("星期六", "1700", "2200"),
-                  this.createBusinessTimeRow("星期日", "1700", "2200"),
-                ])
-              )
-            ])
-        )
-      ]));
+      body: Padding(
+        padding: EdgeInsets.only(bottom: 10),
+        child: ListView(children: [
+          RestaurantHeadCell(imageUrl: widget.detailInfo.image_url ?? ""),
+          RestaurantInfoCell(),
+          RestaurantImageCell(imageUrl: widget.detailInfo.image_url ?? ""),
+          RestaurantBusinessCell(),
+          RestaurantCommentCell()
+        ])
+      ));
 }
