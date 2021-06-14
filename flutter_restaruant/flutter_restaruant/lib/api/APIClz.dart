71d1e3ea5826d77b839f2321a5d12535ca8847e1
Randy <o1984531@gmail.com>
2021-06-15 02:25:54 +0800

    Adjust the generic type from String to specific type

--- flutter_restaruant/flutter_restaruant/lib/api/APIClz.dart
+++ flutter_restaruant/flutter_restaruant/lib/api/APIClz.dart
@@ -1,4 +1,7 @@
 import 'package:flutter_restaruant/api/dio/DioClient.dart';
+import 'package:flutter_restaruant/model/YelpRestaurantDetailInfo.dart';
+import 'package:flutter_restaruant/model/YelpReviewInfo.dart';
+import 'package:flutter_restaruant/model/YelpSearchInfo.dart';
 import 'package:flutter_restaruant/utils/Constants.dart';
 import 'package:logger/logger.dart';
 import 'package:retrofit/retrofit.dart';
@@ -9,45 +12,47 @@ part 'APIClz.g.dart';
 
 @RestApi(baseUrl: Constants.BASE_URL)
 abstract class APIClz {
-  factory APIClz(Dio dio, {String baseUrl}) = _APIClz;
+  factory APIClz(Dio dio, {String? baseUrl}) = _APIClz;
 
   @POST("/oauth2/token")
   @FormUrlEncoded()
   Future<String> fetchToken(
-      @Field("grant_type") String grantType,
-      @Field("client_id") String clientId,
-      @Field("client_secret") String clientSecret);
+      @Field("grant_type") String? grantType,
+      @Field("client_id") String? clientId,
+      @Field("client_secret") String? clientSecret);
 
   @POST("/v3/businesses/search")
-  Future<String> businessesSearch(
-      @Field("term") String term,
-      @Field("latitude") String latitude,
-      @Field("longitude") String longitude,
-      @Field("locale") String locale,
-      @Field("limit") String limit,
-      {@Field("openAt") String openAt,
-      @Field("sortBy") String sortBy,
-      @Field("price") String price});
+  Future<YelpSearchInfo> businessesSearch(
+      @Field("term") String? term,
+      @Field("latitude") String? latitude,
+      @Field("longitude") String? longitude,
+      @Field("locale") String? locale,
+      @Field("limit") String? limit,
+      {@Field("openAt") String? openAt,
+      @Field("sortBy") String? sortBy,
+      @Field("price") String? price});
 
   @GET("/v3/businesses/{locationName}")
-  Future<String> business(
-      @Path() String locationName, @Field("locale") String locale);
+  Future<YelpRestaurantDetailInfo> business(
+      @Path() String? locationName,
+      @Field("locale") String? locale);
 
   @GET("/v3/businesses/{id}/reviews")
-  Future<String> review(@Path() String id, @Field("locale") String locale);
+  Future<YelpReviewInfo> review(@Path() String? id,
+      @Field("locale") String? locale);
 }
 
 final dioClient = DioClient(
     connectionTimeout: Constants.CONNECTION_TIEMOUT,
     receiveTimeout: Constants.RECEIVE_TIEMOUT,
     interceptWraps: [
-      InterceptorsWrapper(onRequest: (RequestOptions options) async {
+      InterceptorsWrapper(onRequest: (options, handler) async {
         var customHeaders = {
           "Content-Type": "application/json",
           "Authorization": Constants.AUTH_TOKEN
         };
         options.headers.addAll(customHeaders);
-        return options;
+        handler.next(options);
       })
     ]);
 final apiInstance = APIClz(dioClient.dio);
--- flutter_restaruant/flutter_restaruant/lib/api/APIClz.g.dart
+++ flutter_restaruant/flutter_restaruant/lib/api/APIClz.g.dart
@@ -8,49 +8,42 @@ part of 'APIClz.dart';
 
 class _APIClz implements APIClz {
   _APIClz(this._dio, {this.baseUrl}) {
-    ArgumentError.checkNotNull(_dio, '_dio');
-    this.baseUrl ??= 'https://api.yelp.com';
+    baseUrl ??= 'https://api.yelp.com';
   }
 
   final Dio _dio;
 
-  String baseUrl;
+  String? baseUrl;
 
   @override
-  fetchToken(grantType, clientId, clientSecret) async {
-    ArgumentError.checkNotNull(grantType, 'grantType');
-    ArgumentError.checkNotNull(clientId, 'clientId');
-    ArgumentError.checkNotNull(clientSecret, 'clientSecret');
+  Future<String> fetchToken(grantType, clientId, clientSecret) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
     final _data = {
       'grant_type': grantType,
       'client_id': clientId,
       'client_secret': clientSecret
     };
-    final Response<String> _result = await _dio.request('/oauth2/token',
-        queryParameters: queryParameters,
-        options: RequestOptions(
+    _data.removeWhere((k, v) => v == null);
+    final _result = await _dio.fetch<String>(_setStreamType<String>(Options(
             method: 'POST',
             headers: <String, dynamic>{},
             extra: _extra,
-            contentType: 'application/x-www-form-urlencoded',
-            baseUrl: baseUrl),
-        data: _data);
-    final value = _result.data;
-    return Future.value(value);
+            contentType: 'application/x-www-form-urlencoded')
+        .compose(_dio.options, '/oauth2/token',
+            queryParameters: queryParameters, data: _data)
+        .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!;
+    return value;
   }
 
   @override
-  businessesSearch(term, latitude, longitude, locale, limit,
+  Future<String> businessesSearch(term, latitude, longitude, locale, limit,
       {openAt, sortBy, price}) async {
-    ArgumentError.checkNotNull(term, 'term');
-    ArgumentError.checkNotNull(latitude, 'latitude');
-    ArgumentError.checkNotNull(longitude, 'longitude');
-    ArgumentError.checkNotNull(locale, 'locale');
-    ArgumentError.checkNotNull(limit, 'limit');
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
     final _data = {
       'term': term,
       'latitude': latitude,
@@ -61,55 +54,58 @@ class _APIClz implements APIClz {
       'sortBy': sortBy,
       'price': price
     };
-    final Response<String> _result = await _dio.request('/v3/businesses/search',
-        queryParameters: queryParameters,
-        options: RequestOptions(
-            method: 'POST',
-            headers: <String, dynamic>{},
-            extra: _extra,
-            baseUrl: baseUrl),
-        data: _data);
-    final value = _result.data;
-    return Future.value(value);
+    _data.removeWhere((k, v) => v == null);
+    final _result = await _dio.fetch<String>(_setStreamType<String>(
+        Options(method: 'POST', headers: <String, dynamic>{}, extra: _extra)
+            .compose(_dio.options, '/v3/businesses/search',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!;
+    return value;
   }
 
   @override
-  business(locationName, locale) async {
-    ArgumentError.checkNotNull(locationName, 'locationName');
-    ArgumentError.checkNotNull(locale, 'locale');
+  Future<String> business(locationName, locale) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
     final _data = {'locale': locale};
-    final Response<String> _result = await _dio.request(
-        '/v3/businesses/$locationName',
-        queryParameters: queryParameters,
-        options: RequestOptions(
-            method: 'GET',
-            headers: <String, dynamic>{},
-            extra: _extra,
-            baseUrl: baseUrl),
-        data: _data);
-    final value = _result.data;
-    return Future.value(value);
+    _data.removeWhere((k, v) => v == null);
+    final _result = await _dio.fetch<String>(_setStreamType<String>(
+        Options(method: 'GET', headers: <String, dynamic>{}, extra: _extra)
+            .compose(_dio.options, '/v3/businesses/$locationName',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!;
+    return value;
   }
 
   @override
-  review(id, locale) async {
-    ArgumentError.checkNotNull(id, 'id');
-    ArgumentError.checkNotNull(locale, 'locale');
+  Future<String> review(id, locale) async {
     const _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
+    queryParameters.removeWhere((k, v) => v == null);
     final _data = {'locale': locale};
-    final Response<String> _result = await _dio.request(
-        '/v3/businesses/$id/reviews',
-        queryParameters: queryParameters,
-        options: RequestOptions(
-            method: 'GET',
-            headers: <String, dynamic>{},
-            extra: _extra,
-            baseUrl: baseUrl),
-        data: _data);
-    final value = _result.data;
-    return Future.value(value);
+    _data.removeWhere((k, v) => v == null);
+    final _result = await _dio.fetch<String>(_setStreamType<String>(
+        Options(method: 'GET', headers: <String, dynamic>{}, extra: _extra)
+            .compose(_dio.options, '/v3/businesses/$id/reviews',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = _result.data!;
+    return value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
   }
 }
