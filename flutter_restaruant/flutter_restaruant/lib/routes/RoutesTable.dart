c1f44d4aea1b8c09bcfe1363851dfb4d6ee3ae4f
Randy <o1984531@gmail.com>
2021-06-29 00:47:29 +0800

    Adjust the page of RoutesTable wrapped by BLoC Provider

--- flutter_restaruant/flutter_restaruant/lib/routes/RoutesTable.dart
+++ flutter_restaruant/flutter_restaruant/lib/routes/RoutesTable.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_restaruant/flow/restaurant/RestaurantDetailPage.dart';
+import 'package:flutter_restaruant/flow/restaurant/bloc/RestaurantDetailBloc.dart';
+import 'package:flutter_restaruant/flow/restaurant/repository/RestaurantDetailRepository.dart';
 
 Map<String, WidgetBuilder> ROUTES_TABLE = <String, WidgetBuilder> {
-  RestaurantDetailPage.ROUTE_NAME: (context) => RestaurantDetailPage()
+  RestaurantDetailPage.ROUTE_NAME: (context) => BlocProvider<RestaurantDetailBloc>(
+      create: (_) => RestaurantDetailBloc(repository: RestaurantDetailRepository()),
+      child: RestaurantDetailPage()
+  )
 };
