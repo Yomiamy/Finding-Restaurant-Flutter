a8662cb7dddd760cd5d7595ba286e197ab84e911
Randy <o1984531@gmail.com>
2021-07-13 13:01:27 +0800

    Add business day to day string mapping function

--- flutter_restaruant/flutter_restaruant/lib/model/YelpBaseInfo.dart
+++ flutter_restaruant/flutter_restaruant/lib/model/YelpBaseInfo.dart
@@ -25,6 +25,26 @@ class YelpBaseInfo {
 
   Image getRatingImage(String rating) => YelpBaseInfo._sRatingImgMap[rating];
 
+  String getWeekDayStrByIndex(int day) {
+    switch(day) {
+      case 0:
+        return "星期一";
+      case 1:
+        return "星期二";
+      case 2:
+        return "星期三";
+      case 3:
+        return "星期四";
+      case 4:
+        return "星期五";
+      case 5:
+        return "星期六";
+      case 6:
+        return "星期日";
+    }
+    return "";
+  }
+
   factory YelpBaseInfo.fromJson(Map<String, dynamic> json) =>
       _$YelpBaseInfoFromJson(json);
 
