8e1a67434ea070aa06f2bde720ed71829f75c360
Randy <o1984531@gmail.com>
2021-07-17 01:18:03 +0800

    Add ActionSheet for navigation and street view

--- flutter_restaruant/flutter_restaruant/lib/component/cell/restaurant_detail/RestaurantInfoCell.dart
+++ flutter_restaruant/flutter_restaruant/lib/component/cell/restaurant_detail/RestaurantInfoCell.dart
@@ -1,23 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_restaruant/model/YelpRestaurantDetailInfo.dart';
+import 'package:flutter_restaruant/utils/Constants.dart';
 import 'package:flutter_restaruant/utils/Dimens.dart';
 import 'package:flutter_restaruant/utils/UIConstants.dart';
+import 'package:flutter_restaruant/utils/Utils.dart';
+import 'package:sprintf/sprintf.dart';
 
 class RestaurantInfoCell extends StatelessWidget {
-
   static const int MAP_IMAGE_W = 140;
   static const int MAP_IMAGE_H = 140;
 
   final YelpRestaurantDetailInfo _detailInfo;
   final String staticMapUrl;
 
-  const RestaurantInfoCell({Key? key = const Key("RestaurantImageCell"),
-    required YelpRestaurantDetailInfo detailInfo,
-    required this.staticMapUrl}) : this._detailInfo = detailInfo,  super(key: key);
+  const RestaurantInfoCell(
+      {Key? key = const Key("RestaurantImageCell"),
+      required YelpRestaurantDetailInfo detailInfo,
+      required this.staticMapUrl})
+      : this._detailInfo = detailInfo,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    String category = this._detailInfo.categories?.map((category) => category.title ?? "").join(" ") ?? "";
+    String category = this
+            ._detailInfo
+            .categories
+            ?.map((category) => category.title ?? "")
+            .join(" ") ?? "";
 
     return Padding(
         padding: EdgeInsets.only(left: 5, right: 5, top: 10),
@@ -25,16 +35,21 @@ class RestaurantInfoCell extends StatelessWidget {
           SizedBox(
               width: RestaurantInfoCell.MAP_IMAGE_H.toDouble(),
               height: RestaurantInfoCell.MAP_IMAGE_W.toDouble(),
-              child: FadeInImage.assetNetwork(
-                  placeholder: UIConstants.NO_IMAGE,
-                  imageErrorBuilder: (context, error, trace) =>
-                      Image.asset(UIConstants.NO_IMAGE),
-                  image: this.staticMapUrl,
-                  imageCacheHeight: RestaurantInfoCell.MAP_IMAGE_H,
-                  imageCacheWidth: RestaurantInfoCell.MAP_IMAGE_W,
-                  placeholderCacheHeight: RestaurantInfoCell.MAP_IMAGE_H,
-                  placeholderCacheWidth: RestaurantInfoCell.MAP_IMAGE_W,
-                  fit: BoxFit.fill)),
+              child: GestureDetector(
+                  onTap: () {
+                    showCupertinoModalPopup(
+                        context: context, builder: buildNavigationActionSheet);
+                  },
+                  child: FadeInImage.assetNetwork(
+                      placeholder: UIConstants.NO_IMAGE,
+                      imageErrorBuilder: (context, error, trace) =>
+                          Image.asset(UIConstants.NO_IMAGE),
+                      image: this.staticMapUrl,
+                      imageCacheHeight: RestaurantInfoCell.MAP_IMAGE_H,
+                      imageCacheWidth: RestaurantInfoCell.MAP_IMAGE_W,
+                      placeholderCacheHeight: RestaurantInfoCell.MAP_IMAGE_H,
+                      placeholderCacheWidth: RestaurantInfoCell.MAP_IMAGE_W,
+                      fit: BoxFit.fill))),
           Expanded(
               child: Container(
                   padding: EdgeInsets.only(left: 10),
@@ -75,4 +90,40 @@ class RestaurantInfoCell extends StatelessWidget {
                       ])))
         ]));
   }
+
+  CupertinoActionSheet buildNavigationActionSheet(BuildContext context) =>
+      CupertinoActionSheet(
+          title: Text("請選擇導覽方式"),
+          cancelButton: CupertinoActionSheetAction(
+              isDestructiveAction: true,
+              child: Text("取消"),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          actions: [
+            CupertinoActionSheetAction(
+                child: Text("導航"),
+                onPressed: () {
+                  double lat = this._detailInfo.coordinates?.latitude ?? 0;
+                  double lng = this._detailInfo.coordinates?.longitude ?? 0;
+
+                  Utils.openUrl(
+                      scheme: Constants.HTTPS_SCHEME,
+                      host: Constants.GOOGLE_MAP_HOST,
+                      path: Constants.GOOGLE_MAP_NAVIGATION_PATH, parameters: <String, String> {
+                        Constants.GOOGLE_MAP_NAVIGATION_PARAMETER : sprintf("%f,%f", [lat, lng]),
+                  });
+                  Navigator.pop(context);
+                }),
+            CupertinoActionSheetAction(
+                isDefaultAction: false,
+                child: Text("街景視圖"),
+                onPressed: () {
+                  // let panormaViewController = PanoramaViewController()
+                  // panormaViewController.mLat = lat!
+                  // panormaViewController.mLng = lng!
+                  // self.navigationController?.pushViewController(panormaViewController, animated: true)
+                  Navigator.pop(context);
+                })
+          ]);
 }
