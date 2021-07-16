adc2b547bfa7bb97923c11ee02b8bed548ccd0b6
Randy <o1984531@gmail.com>
2021-07-16 17:59:08 +0800

    Add static map api utility

--- flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
+++ flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
@@ -1,6 +1,8 @@
 class Constants {
   /// [API]
   static const BASE_URL = "https://api.yelp.com";
+  static const GOOGLE_MAP_API_URL = "https://maps.googleapis.com/maps/api";
+  static const GOOGLE_STATIC_MAP = "$GOOGLE_MAP_API_URL/staticmap";
   static const AUTH_TOKEN = "Bearer 4htbz9nLozJ_-Xw-13LpeVWrEIRWZt4IrgTOQXstx7M1DCVeUIoxIZkQ0XVLVEtD2Gy2Vp1FjA2WRz5DOpZMSxHfXVBLR3gi0DeMXIV3X1bCHYoMoJ-_TZLfBn0ZWnYx";
   static const LOCALE = "zh_TW";
   static const int CONNECTION_TIEMOUT = 30000;
