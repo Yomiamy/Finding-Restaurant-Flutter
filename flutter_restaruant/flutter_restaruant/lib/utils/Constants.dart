569ca59eda70d49acc6b28eef15b4a6be5d2d591
Randy <o1984531@gmail.com>
2021-07-17 01:15:45 +0800

    Add constants for google map

--- flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
+++ flutter_restaruant/flutter_restaruant/lib/utils/Constants.dart
@@ -3,11 +3,14 @@ class Constants {
   static const BASE_URL = "https://api.yelp.com";
   static const GOOGLE_MAP_API_URL = "https://maps.googleapis.com/maps/api";
   static const GOOGLE_STATIC_MAP = "$GOOGLE_MAP_API_URL/staticmap";
+  static const HTTPS_SCHEME = "https";
+  static const GOOGLE_MAP_HOST = "maps.google.de";
+  static const GOOGLE_MAP_NAVIGATION_PATH = "/maps";
+  static const GOOGLE_MAP_NAVIGATION_PARAMETER = "q";
   static const AUTH_TOKEN = "Bearer 4htbz9nLozJ_-Xw-13LpeVWrEIRWZt4IrgTOQXstx7M1DCVeUIoxIZkQ0XVLVEtD2Gy2Vp1FjA2WRz5DOpZMSxHfXVBLR3gi0DeMXIV3X1bCHYoMoJ-_TZLfBn0ZWnYx";
   static const LOCALE = "zh_TW";
   static const int CONNECTION_TIEMOUT = 30000;
   static const int RECEIVE_TIEMOUT = 30000;
   static const int PAGE_ITEM_COUNT = 50;
-
 
 }
