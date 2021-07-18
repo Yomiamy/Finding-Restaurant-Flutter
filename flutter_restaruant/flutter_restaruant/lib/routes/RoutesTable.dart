b33b9c68f7b4f8ce3f466e3da5407bf4a9169b1a
Randy <o1984531@gmail.com>
2021-07-19 02:26:59 +0800

    Add the route for FilterPage

--- flutter_restaruant/flutter_restaruant/lib/routes/RoutesTable.dart
+++ flutter_restaruant/flutter_restaruant/lib/routes/RoutesTable.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_restaruant/flow/filter/view/FilterPage.dart';
 import 'package:flutter_restaruant/flow/restaurant/view/RestaurantDetailPage.dart';
 import 'package:flutter_restaruant/flow/restaurant/bloc/RestaurantDetailBloc.dart';
 import 'package:flutter_restaruant/flow/restaurant/repository/RestaurantDetailRepository.dart';
@@ -8,5 +9,6 @@ Map<String, WidgetBuilder> ROUTES_TABLE = <String, WidgetBuilder> {
   RestaurantDetailPage.ROUTE_NAME: (context) => BlocProvider<RestaurantDetailBloc>(
       create: (_) => RestaurantDetailBloc(repository: RestaurantDetailRepository()),
       child: RestaurantDetailPage()
-  )
+  ),
+  FilterPage.ROUTE_NAME: (context) => FilterPage()
 };
