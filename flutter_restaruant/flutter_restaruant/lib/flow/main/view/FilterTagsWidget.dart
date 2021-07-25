a596ceb5aa86d952fb3e8e9da18229e4eedf193b
Randy <o1984531@gmail.com>
2021-07-25 11:54:13 +0800

    Add the FilterTagsWidget

---
+++ flutter_restaruant/flutter_restaruant/lib/flow/main/view/FilterTagsWidget.dart
@@ -0,0 +1,57 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_restaruant/flow/main/bloc/MainBloc.dart';
+import 'package:flutter_restaruant/model/FilterConfigs.dart';
+import 'package:flutter_restaruant/utils/Dimens.dart';
+import 'package:flutter_tags/flutter_tags.dart';
+
+import 'MainPage.dart';
+
+class FilterTagsWidget extends StatelessWidget {
+
+  final GlobalKey<TagsState> _tagStateKey = GlobalKey<TagsState>();
+
+  final FilterConfigs _filterConfigs;
+  final LinkedHashMap<FilterConfigType, String> _filterConfigsMap= LinkedHashMap<FilterConfigType,String>();
+
+  FilterTagsWidget({Key? key, required filterConfigs}) : this._filterConfigs = filterConfigs, super(key: key) {
+    if (this._filterConfigs.sortBy.isNotEmpty) {
+      this._filterConfigsMap[FilterConfigType.SORTING_RULE] = this._filterConfigs.sortingRuleDispStr;
+    }
+
+    if (this._filterConfigs.price > 0) {
+      this._filterConfigsMap[FilterConfigType.PRICE] = this._filterConfigs.priceDispStr;
+    }
+
+    if (this._filterConfigs.openAt > 0) {
+      this._filterConfigsMap[FilterConfigType.OPEN_AT] = this._filterConfigs.openAtDispStr;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) => SizedBox(
+    height: 50,
+    child: Center(
+      child: Tags(
+          key: this._tagStateKey,
+          itemCount: this._filterConfigsMap.keys.length,
+          itemBuilder: (int index) => ItemTags(
+              key: Key(index.toString()),
+              index: index,
+              title: this._filterConfigsMap.values.toList()[index],
+              textStyle: TextStyle( fontSize: Dimens.xhFontSize),
+              customData: this._filterConfigsMap.keys.toList()[index],
+              combine: ItemTagsCombine.withTextBefore,
+              color: Theme.of(context).primaryColor,
+              onPressed: (item) {
+                debugPrint('item = ${item}');
+                context.findAncestorStateOfType<MainPageState>()?.setState(() {
+                  this._filterConfigs.clearConfig(item.customData as FilterConfigType);
+                });
+              }
+          )
+      )
+    )
+  );
+}
