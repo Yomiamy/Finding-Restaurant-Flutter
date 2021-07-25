47a0555fea22fe43fb4c4aa85da04656f91b1a7f
Randy <o1984531@gmail.com>
2021-07-25 11:53:58 +0800

    Add the FilterConfigType and adjust the index field

--- flutter_restaruant/flutter_restaruant/lib/model/FilterConfigs.dart
+++ flutter_restaruant/flutter_restaruant/lib/model/FilterConfigs.dart
@@ -1,34 +1,33 @@
+import 'package:intl/intl.dart';
+
+enum FilterConfigType {
+  PRICE,
+  OPEN_AT,
+  SORTING_RULE
+}
+
 class FilterConfigs {
+  // Price
   int price = 1;
+  int get priceIndex => this.price < 1 ? 0 : (this.price - 1);
+
   String get priceDispStr {
     switch (this.price) {
       case 1: return "\$";
       case 2: return "\$\$";
       case 3: return "\$\$\$";
       case 4: return "\$\$\$\$";
-      default: return "\$";
+      default: return "";
     }
   }
-  int get priceIndex => this.price - 1;
 
+  // Business hours
   int openAt = DateTime.now().millisecondsSinceEpoch;
-  DateTime get openAtDateTime => DateTime.fromMillisecondsSinceEpoch(this.openAt);
+  DateTime get openAtDateTime => (this.openAt > 0) ? DateTime.fromMillisecondsSinceEpoch(this.openAt) : DateTime.now();
+  String get openAtDispStr => DateFormat('MM-dd HH:mm').format(this.openAtDateTime);
 
+  // Sorting rule
   String sortBy = "best_match";
-  // String get sortingRuleDispStr {
-  //   switch(this.sortingRule) {
-  //     case "best_match":
-  //       return "BestMatch";
-  //     case "distance":
-  //       return "Distance";
-  //     case "review_count":
-  //       return "ReviewCount";
-  //     case "rating":
-  //       return "Rating";
-  //     default:
-  //       return "BestMatch";
-  //   }
-  // }
   int get sortByIndex {
     switch(this.sortBy) {
       case "best_match":
@@ -43,7 +42,20 @@ class FilterConfigs {
         return 0;
     }
   }
-
+  String get sortingRuleDispStr {
+    switch(this.sortBy) {
+      case "best_match":
+        return "BestMatch";
+      case "distance":
+        return "Distance";
+      case "review_count":
+        return "ReviewCount";
+      case "rating":
+        return "Rating";
+      default:
+        return "";
+    }
+  }
   String mapSortingRuleByIndex(int sortByIndex) {
     switch (sortByIndex) {
       case 0:
@@ -65,5 +77,19 @@ class FilterConfigs {
     this.price = priceIndex + 1;
     this.openAt = openAtDate.millisecondsSinceEpoch;
     this.sortBy = this.mapSortingRuleByIndex(sortingRuleIndex);
+  }
+
+  void clearConfig(FilterConfigType configType) {
+    switch(configType) {
+      case FilterConfigType.PRICE:
+        this.price = -1;
+        break;
+      case FilterConfigType.OPEN_AT:
+        this.openAt = -1;
+        break;
+      case FilterConfigType.SORTING_RULE:
+        this.sortBy = "";
+        break;
+    }
   }
 }
