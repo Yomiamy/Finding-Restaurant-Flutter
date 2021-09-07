bf0bdf4fdae8558e1444329230dd58da27d6c2df
Randy <o1984531@gmail.com>
2021-09-08 02:45:20 +0800

    Add the load more when reach to the edge of ListView

--- flutter_restaruant/flutter_restaruant/lib/flow/main/repository/MainRepository.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/main/repository/MainRepository.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_restaruant/api/APIClz.dart';
+import 'package:flutter_restaruant/model/YelpRestaurantSummaryInfo.dart';
 import 'package:flutter_restaruant/model/YelpSearchInfo.dart';
 import 'package:flutter_restaruant/utils/Constants.dart';
 
@@ -7,18 +8,19 @@ class MainRepository {
   static const int _MAX_ITEMS_COUNT_IN_LIST = 50;
 
   late int _offset;
+  late List<YelpRestaurantSummaryInfo> summaryInfos;
 
-  MainRepository() {}
+  MainRepository() {
+    this._offset = 0;
+    this.summaryInfos = [];
+  }
 
   void resetOffset() {
     this._offset = 0;
   }
 
-  Future<YelpSearchInfo> fetchYelpSearchInfo(double lat, double lng, int? price,
-      int? openAt, String? sortBy) async {
-    this._offset++;
-
-    return apiInstance.businessesSearch(
+  Future<List<YelpRestaurantSummaryInfo>> fetchYelpSearchInfo(double lat, double lng, int? price, int? openAt, String? sortBy) async {
+    YelpSearchInfo searchInfo = await apiInstance.businessesSearch(
         term: "Restaurants",
         latitude: lat,
         longitude: lng,
@@ -27,6 +29,10 @@ class MainRepository {
         openAt: openAt,
         sortBy: sortBy,
         limit: MainRepository._MAX_ITEMS_COUNT_IN_LIST,
-        offset: this._offset);
+        offset: ++this._offset);
+
+    this.summaryInfos.addAll(searchInfo.businesses ?? []);
+
+    return this.summaryInfos;
   }
 }
