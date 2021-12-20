cbbb388d38a633418290d77d20e7000126a3303d
Randy <o1984531@gmail.com>
2021-12-20 23:40:27 +0800

    [add] Initial the Firebase

--- flutter_restaruant/flutter_restaruant/lib/main.dart
+++ flutter_restaruant/flutter_restaruant/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
@@ -5,10 +6,17 @@ import 'package:flutter_restaruant/flow/main/bloc/MainBloc.dart';
 import 'package:flutter_restaruant/flow/main/repository/MainRepository.dart';
 import 'package:flutter_restaruant/flow/splash/view/SplashPage.dart';
 import 'package:flutter_restaruant/utils/Constants.dart';
+import 'firebase_options.dart';
+import 'flow/login/view/LoginPage.dart';
 import 'flow/main/view/MainPage.dart';
 import 'routes/RoutesTable.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(FindingRestaruantApp());
 }
 
