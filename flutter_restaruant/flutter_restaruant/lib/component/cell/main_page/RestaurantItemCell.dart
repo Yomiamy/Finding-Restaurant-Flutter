fd280c607f7b24ce17bec7ccf5332c11b911e7c9
Randy <o1984531@gmail.com>
2022-02-08 00:30:56 +0800

    Adjust the favorite icon position

--- flutter_restaruant/flutter_restaruant/lib/component/cell/main_page/RestaurantItemCell.dart
+++ flutter_restaruant/flutter_restaruant/lib/component/cell/main_page/RestaurantItemCell.dart
@@ -12,101 +12,119 @@ class RestaurantItemCell extends StatelessWidget {
   final YelpRestaurantSummaryInfo _summaryInfo;
   final bool _isFavorPage;
 
-  const RestaurantItemCell({required YelpRestaurantSummaryInfo summaryInfo, required bool isFavorPage}): this._summaryInfo = summaryInfo, this._isFavorPage = isFavorPage;
+  const RestaurantItemCell(
+      {required YelpRestaurantSummaryInfo summaryInfo,
+      required bool isFavorPage})
+      : this._summaryInfo = summaryInfo,
+        this._isFavorPage = isFavorPage;
 
   @override
   Widget build(BuildContext context) {
     String category = this._summaryInfo.categoriesStr;
 
     return SizedBox(
-      height: 100,
-      child: Container(
-          padding: EdgeInsets.only(left: 10, right: 5, top: 10, bottom: 0),
-          child: Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
-            SizedBox(
-                width: RestaurantItemCell.IMAGE_W.toDouble(),
-                height: RestaurantItemCell.IMAGE_H.toDouble(),
-                child: Stack(
-                  children: <Widget>[
-                    Align(
-                      alignment:Alignment.center,
-                      child: SizedBox.expand(
-                        child: FadeInImage.assetNetwork(
-                            placeholder: UIConstants.NO_IMAGE,
-                            imageErrorBuilder: (context, error, trace) => Image.asset(UIConstants.NO_IMAGE),
-                            image: this._summaryInfo.image_url ?? "",
-                            imageCacheHeight: RestaurantItemCell.IMAGE_H,
-                            imageCacheWidth: RestaurantItemCell.IMAGE_W,
-                            placeholderCacheHeight: RestaurantItemCell.IMAGE_H,
-                            placeholderCacheWidth: RestaurantItemCell.IMAGE_W,
-                            fit: BoxFit.fill)
+        height: 100,
+        child: Container(
+            padding: EdgeInsets.only(left: 10, right: 5, top: 10, bottom: 0),
+            child: Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
+              SizedBox(
+                  width: RestaurantItemCell.IMAGE_W.toDouble(),
+                  height: RestaurantItemCell.IMAGE_H.toDouble(),
+                  child: FadeInImage.assetNetwork(
+                      placeholder: UIConstants.NO_IMAGE,
+                      imageErrorBuilder: (context, error, trace) =>
+                          Image.asset(UIConstants.NO_IMAGE),
+                      image: this._summaryInfo.image_url ?? "",
+                      imageCacheHeight: RestaurantItemCell.IMAGE_H,
+                      imageCacheWidth: RestaurantItemCell.IMAGE_W,
+                      placeholderCacheHeight: RestaurantItemCell.IMAGE_H,
+                      placeholderCacheWidth: RestaurantItemCell.IMAGE_W,
+                      fit: BoxFit.fill)),
+              Expanded(
+                  child: Container(
+                      padding: EdgeInsets.only(left: 10),
+                      child: SizedBox(
+                          height: RestaurantItemCell.ITEM_H,
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Flex(
+                                    direction: Axis.horizontal,
+                                    children: <Widget>[
+                                      Expanded(
+                                          child: Text(
+                                              this._summaryInfo.name ?? "",
+                                              overflow: TextOverflow.ellipsis)),
+                                      Text(
+                                          sprintf("%.2fm",
+                                              [this._summaryInfo.distance]),
+                                          style: TextStyle(
+                                              fontSize: Dimens.mFontSize,
+                                              color: Colors.grey))
+                                    ]),
+                                Flex(direction: Axis.horizontal, children: <
+                                    Widget>[
+                                  Expanded(
+                                      child: this._summaryInfo.getRatingImage(
+                                          this._summaryInfo.rating.toString()),
+                                      flex: 1),
+                                  Expanded(
+                                      child: Align(
+                                          child: Text(
+                                              "${this._summaryInfo.review_count}則評論",
+                                              style: TextStyle(
+                                                  fontSize: Dimens.mFontSize,
+                                                  color: Colors.grey)),
+                                          alignment: Alignment.centerRight),
+                                      flex: 1),
+                                  Expanded(
+                                      child: Align(
+                                          child: Text(
+                                              this._summaryInfo.price ?? "",
+                                              style: TextStyle(
+                                                  fontSize: Dimens.mFontSize,
+                                                  color: Colors.grey)),
+                                          alignment: Alignment.centerRight),
+                                      flex: 1)
+                                ]),
+                                Text(
+                                    this
+                                            ._summaryInfo
+                                            .location
+                                            ?.display_address
+                                            ?.join("") ??
+                                        "",
+                                    overflow: TextOverflow.ellipsis),
+                                Row(
+                                  children: <Widget>[
+                                    Padding(padding: EdgeInsets.only(left: 5),
+                                        child: Visibility(
+                                          child: Image.asset(
+                                              (this._summaryInfo.favor != null) && (this._summaryInfo.favor!)
+                                                  ? "images/ic_favor_fill.png"
+                                                  : "images/ic_favor_empty.png",
+                                              width: UIConstants.FAVOR_IMAGE_W,
+                                              height: UIConstants.FAVOR_IMAGE_H,
+                                              fit: BoxFit.fill),
+                                          visible: !this._isFavorPage,
+                                        )),
+                                    Padding(
+                                        padding: EdgeInsets.only(left: 5, right: 5),
+                                        child: Text(category,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                                fontSize: Dimens.mFontSize,
+                                                color: Colors.grey))
+                                    )
+                                  ]
+                                )
+                              ])
                       )
-                    ),
-                    Visibility(
-                      child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: Padding(
-                              padding: EdgeInsets.only(right: 8, bottom: 5),
-                              child: Image.asset(
-                                  (this._summaryInfo.favor != null) &&
-                                          (this._summaryInfo.favor!)
-                                      ? "images/ic_favor_fill.png"
-                                      : "images/ic_favor_empty.png",
-                                  width: UIConstants.FAVOR_IMAGE_W,
-                                  height: UIConstants.FAVOR_IMAGE_H,
-                                  fit: BoxFit.fill))),
-                      visible: !this._isFavorPage,
-                    )
-                  ]
-                )),
-            Expanded(
-                child: Container(
-                    padding: EdgeInsets.only(left: 10),
-                    child: SizedBox(
-                        height: RestaurantItemCell.ITEM_H,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Flex(direction: Axis.horizontal, children: <Widget>[
-                                Expanded(
-                                    child: Text(this._summaryInfo.name ?? "",
-                                        overflow: TextOverflow.ellipsis)),
-                                Text(sprintf("%.2fm", [this._summaryInfo.distance]),
-                                    style: TextStyle(
-                                        fontSize: Dimens.mFontSize,
-                                        color: Colors.grey))
-                              ]),
-                              Flex(direction: Axis.horizontal, children: <Widget>[
-                                Expanded(
-                                    child: this._summaryInfo.getRatingImage(this._summaryInfo.rating.toString()),
-                                    flex: 1),
-                                Expanded(
-                                    child: Align(
-                                        child: Text("${this._summaryInfo.review_count}則評論",
-                                            style: TextStyle(
-                                                fontSize: Dimens.mFontSize,
-                                                color: Colors.grey)),
-                                        alignment: Alignment.centerRight),
-                                    flex: 1),
-                                Expanded(
-                                    child: Align(
-                                        child: Text(this._summaryInfo.price ?? "",
-                                            style: TextStyle(
-                                                fontSize: Dimens.mFontSize,
-                                                color: Colors.grey)),
-                                        alignment: Alignment.centerRight),
-                                    flex: 1)
-                              ]),
-                              Text(this._summaryInfo.location?.display_address?.join("") ?? "",
-                                  overflow: TextOverflow.ellipsis),
-                              Text(category,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: TextStyle(
-                                      fontSize: Dimens.mFontSize,
-                                      color: Colors.grey))
-                            ]))))
-          ]))
+                  )
+              )
+            ])
+        )
     );
   }
 }
