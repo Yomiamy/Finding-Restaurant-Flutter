245101340b8631e5e4f357422041b0e08095da69
Randy <o1984531@gmail.com>
2022-02-12 00:08:47 +0800

    Adjust the naming and default value

--- flutter_restaruant/flutter_restaruant/lib/flow/favor/repository/FavorRepository.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/favor/repository/FavorRepository.dart
@@ -10,26 +10,24 @@ class FavorRepository {
                                               .collection(_sFavorCollectionName)
                                               .doc(uid)
                                               .get();
-    Map<String, dynamic> favorMap = snapshots.data() as Map<String, dynamic>;
+    Map<String, dynamic> favorMap = (snapshots.data() != null) ? snapshots.data() as Map<String, dynamic> : Map<String, dynamic>();
     return favorMap.values.map((value) => YelpRestaurantSummaryInfo.fromJson(value)).toList();
   }
 
   Future<void> updateFavorInfo(String uid, YelpRestaurantSummaryInfo summaryInfo) async {
-    DocumentReference ref = await FirebaseFirestore.instance
-        .collection(_sFavorCollectionName)
-        .doc(uid);
+    DocumentReference ref = FirebaseFirestore.instance.collection(_sFavorCollectionName).doc(uid);
     DocumentSnapshot snapshots = await ref.get();
-    Map<String, dynamic> favorMap = snapshots.data() as Map<String, dynamic>;
+    Map<String, dynamic> favorsMap = snapshots.data() as Map<String, dynamic>;
     bool isFavor = summaryInfo.favor ?? false;
 
     if (isFavor) {
       // 若設定為最愛資料則新增
-      favorMap[summaryInfo.id!] = summaryInfo.toJson();
+      favorsMap[summaryInfo.id!] = summaryInfo.toJson();
     } else {
       // 若設定為非最愛資料則刪除
-      favorMap.remove(summaryInfo.id!);
+      favorsMap.remove(summaryInfo.id!);
     }
     // 更新資料
-    ref.set(favorMap);
+    ref.set(favorsMap);
   }
 }
