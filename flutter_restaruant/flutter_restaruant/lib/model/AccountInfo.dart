b1d7902e61d118641cef6296041689481ca3f0ae
Randy <o1984531@gmail.com>
2022-02-20 22:51:09 +0800

    Add the “MAIL” account type

--- flutter_restaruant/flutter_restaruant/lib/model/AccountInfo.dart
+++ flutter_restaruant/flutter_restaruant/lib/model/AccountInfo.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 part 'AccountInfo.g.dart';
 
 enum AccountType {
-  GOOGLE, FACEBOOK, APPLE, NONE
+  GOOGLE, FACEBOOK, APPLE, MAIL, NONE
 }
 
 @JsonSerializable()
