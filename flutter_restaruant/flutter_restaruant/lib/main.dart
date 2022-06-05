73399ca71364d6bfcd1b17a4bb8cac8d4fba8b52
Randy <o1984531@gmail.com>
2022-06-06 01:59:12 +0800

    Register the foreground message and opening app callback

--- flutter_restaruant/flutter_restaruant/lib/main.dart
+++ flutter_restaruant/flutter_restaruant/lib/main.dart
@@ -6,19 +6,33 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'component/ad/BannerADState.dart';
 import 'package:flutter_restaruant/utils/UIConstants.dart';
 import 'firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'routes/RoutesTable.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'utils/UIConstants.dart';
 
+
+Future<void> _firebaseMessagingOpenHandler(RemoteMessage message) async {
+  print("Handling a message open: ${message.messageId}");
+}
+
+void _firebaseMessagingForgroundHandler(RemoteMessage message) {
+  print("Handling a foreground message: ${message.messageId}");
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   Constants.init();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FirebaseMessaging.onMessage.listen(_firebaseMessagingForgroundHandler);
+  FirebaseMessaging.onMessageOpenedApp.listen(_firebaseMessagingOpenHandler);
+
   final initFuture = MobileAds.instance.initialize();
   final adState = BannerADState(initFuture);
 
