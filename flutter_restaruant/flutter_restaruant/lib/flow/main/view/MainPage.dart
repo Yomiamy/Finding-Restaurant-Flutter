3a3c53386a90831ed80511fd0ba15fc940ce7d1a
Randy <o1984531@gmail.com>
2024-03-17 02:44:46 +0800

    Revert "fix: Remove useless handleNotificationData"
    This reverts commit 3a8540e37118c00bd08c8f5944ee7f9499191f0c.

--- flutter_restaruant/flutter_restaruant/lib/flow/main/view/MainPage.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/main/view/MainPage.dart
@@ -90,6 +90,7 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
                           if(state is MainInitial) {
                             // Request FCM
                             this._mainBloc.add(NotificationSetup());
+                            this.handleNotificationData();
                           }
                           this._mainBloc.add(FetchSearchInfo(price: price, openAt: openAt, sortBy: sortBy));
                         }
@@ -178,6 +179,23 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
           ]
         )
     );
+  }
+
+  /// --- FCM notification
+  void handleNotificationData() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      // Waiting building is finish and run.
+      final args = ModalRoute.of(context)?.settings.arguments as Tuple2<YelpRestaurantSummaryInfo, dynamic>?;
+      YelpRestaurantSummaryInfo? summaryInfoFromNotification = args?.item1;
+
+      if(summaryInfoFromNotification == null) {
+        return;
+      }
+
+      Tuple2 arguments = Tuple2<YelpRestaurantSummaryInfo, dynamic>(summaryInfoFromNotification, null);
+      // Avoid duplicate push, use pushNamedAndRemoveUntil instead of push
+      Navigator.of(context).pushNamedAndRemoveUntil(RestaurantDetailPage.ROUTE_NAME, ModalRoute.withName(MainPage.ROUTE_NAME), arguments: arguments);
+    });
   }
 
   /// --- AD
