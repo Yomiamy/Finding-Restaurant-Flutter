6475437e7408021a4e450a940b625d23ece73fbe
Yianday <yianday0330h@gmail.com>
2026-07-21 18:10:25 +0800

    refactor(ui): resolve flutter analyzer warnings across view components

--- flutter_restaruant/flutter_restaruant/lib/flow/main/view/FilterTagsWidget.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/main/view/FilterTagsWidget.dart
@@ -55,7 +55,7 @@ class FilterTagsWidget extends StatelessWidget {
                   onPressed: (item) {
                     context
                         .findAncestorStateOfType<MainPageState>()
-                        ?.setState(() {
+                        ?.updateState(() {
                       MainBloc mainBloc = BlocProvider.of<MainBloc>(context);
 
                       this
