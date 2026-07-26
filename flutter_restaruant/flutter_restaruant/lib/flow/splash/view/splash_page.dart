f29c6b54e77f575befd5eabe4cb7350844058498
Yianday <yianday0330h@gmail.com>
2026-07-26 15:04:36 +0800

    style(splash): apply strict linting rules and format
    - update to relative imports
    - apply camelCase to routeName constant
    - add super.key and return types
    - ignore unawaited_futures
    - remove unnecessary Container

--- flutter_restaruant/flutter_restaruant/lib/flow/splash/view/splash_page.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/splash/view/splash_page.dart
@@ -1,13 +1,14 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_restaruant/flow/signinup/view/sign_in_page.dart';
+import '../../signinup/view/sign_in_page.dart';
 
 class SplashPage extends StatefulWidget {
-  static const ROUTE_NAME = "/";
+  static const routeName = '/';
 
-  const SplashPage();
+  const SplashPage({super.key});
 
   @override
-  _SplashPageState createState() => _SplashPageState();
+  State<SplashPage> createState() => _SplashPageState();
 }
 
 class _SplashPageState extends State<SplashPage> {
@@ -16,9 +17,10 @@ class _SplashPageState extends State<SplashPage> {
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
       // Waiting building is finish and run.
-      await Future.delayed(Duration(seconds: 3));
+      await Future.delayed(const Duration(seconds: 3));
       if (mounted) {
-        Navigator.of(context).pushReplacementNamed(SignInPage.ROUTE_NAME);
+        // ignore: unawaited_futures
+        Navigator.of(context).pushReplacementNamed(SignInPage.routeName);
       }
     });
   }
@@ -26,12 +28,11 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Image.asset(
-      "images/launch_image.png",
+        body: Image.asset(
+      'images/launch_image.png',
       fit: BoxFit.fill,
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-    )));
+    ));
   }
 }
