eac32b7b257c0e3ce782aba92d233c9821cb38c7
Yianday <yianday0330h@gmail.com>
2026-07-27 22:58:43 +0800

    refactor(main): migrate main page views to RestaurantEntity
    Replace YelpRestaurantSummaryInfo with RestaurantEntity across MainPage
    and RestaurantInfoListWidget, and resolve BannerADState from get_it
    instead of the removed provider lookup. Modernize touched lines along
    the way (relative imports, camelCase routeName, const constructors,
    single quotes, this. removal).
    
    Both files ship together because MainPage constructs
    RestaurantInfoListWidget and the element type must match.

--- flutter_restaruant/flutter_restaruant/lib/flow/main/view/main_page.dart
+++ flutter_restaruant/flutter_restaruant/lib/flow/main/view/main_page.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:flutter_restaruant/component/empty_data_widget.dart';
-import 'package:flutter_restaruant/component/loading_widget.dart';
-import 'package:flutter_restaruant/component/ad/app_open_ad_state.dart';
-import 'package:flutter_restaruant/flow/favor/view/favor_page.dart';
-import 'package:flutter_restaruant/flow/filter/view/filter_page.dart';
-import 'package:flutter_restaruant/flow/main/view/map_widget.dart';
-import 'package:flutter_restaruant/flow/main/view/restaurant_info_list_widget.dart';
-import 'package:flutter_restaruant/flow/restaurant/view/restaurant_detail_page.dart';
-import 'package:flutter_restaruant/flow/settings/view/settings_page.dart';
-import 'package:flutter_restaruant/generated/l10n.dart';
-import 'package:flutter_restaruant/model/filter_configs.dart';
-import 'package:flutter_restaruant/model/yelp_restaurant_summary_info.dart';
-import 'package:flutter_restaruant/utils/tuple.dart';
-import 'package:flutter_restaruant/utils/ui_constants.dart';
-import 'package:flutter_restaruant/utils/view_utils.dart';
+import '../../../component/empty_data_widget.dart';
+import '../../../component/loading_widget.dart';
+import '../../../component/ad/app_open_ad_state.dart';
+import '../../../domain/entities/restaurant_entity.dart';
+import '../../favor/view/favor_page.dart';
+import '../../filter/view/filter_page.dart';
+import 'map_widget.dart';
+import 'restaurant_info_list_widget.dart';
+import '../../restaurant/view/restaurant_detail_page.dart';
+import '../../settings/view/settings_page.dart';
+import '../../../generated/l10n.dart';
+import '../../../model/filter_configs.dart';
+import '../../../utils/tuple.dart';
+import '../../../utils/ui_constants.dart';
+import '../../../utils/view_utils.dart';
 
 import '../bloc/main_bloc.dart';
-import 'package:flutter_restaruant/gen/colors.gen.dart';
+import '../../../gen/colors.gen.dart';
 
 class MainPage extends StatefulWidget {
-  static const ROUTE_NAME = "/MainPage";
+  static const routeName = '/MainPage';
 
-  MainPage({Key key = const Key("MainPage")}) : super(key: key);
+  const MainPage({Key key = const Key('MainPage')}) : super(key: key);
 
   @override
   MainPageState createState() => MainPageState();
@@ -31,11 +31,11 @@ class MainPage extends StatefulWidget {
 
 class MainPageState extends State<MainPage> implements AppOpenADEvent {
   FilterConfigs _configs = FilterConfigs();
-  String _filterKeyword = "";
+  String _filterKeyword = '';
   bool _isListMode = true;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   late MainBloc _mainBloc;
-  late List<YelpRestaurantSummaryInfo> _summaryInfos;
+  late List<RestaurantEntity> _summaryInfos;
 
   MainPageState();
 
@@ -43,16 +43,16 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
   void initState() {
     super.initState();
 
-    this._mainBloc = BlocProvider.of<MainBloc>(context);
-    this._summaryInfos = List.empty();
+    _mainBloc = BlocProvider.of<MainBloc>(context);
+    _summaryInfos = List.empty();
 
-    this._mainBloc.add(NotificationSetup());
-    this._mainBloc.add(FetchSearchInfo(
-        price: this._configs.price,
-        openAt: this._configs.openAtInSec,
-        sortBy: this._configs.sortBy));
+    _mainBloc.add(const NotificationSetup());
+    _mainBloc.add(FetchSearchInfo(
+        price: _configs.price,
+        openAt: _configs.openAtInSec,
+        sortBy: _configs.sortBy));
 
-    this.handleNotificationData();
+    handleNotificationData();
   }
 
   @override
@@ -78,27 +78,27 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
         automaticallyImplyLeading: false,
         title: Text(
           S.current.main_page_title,
-          style: TextStyle(
+          style: const TextStyle(
               color: Colors.white, fontSize: UIConstants.xxxxhFontSize),
         ),
         backgroundColor: ColorName.appPrimaryColor,
         leading: IconButton(
             padding: EdgeInsets.zero,
             onPressed: () => _openDrawer(),
-            icon: Icon(Icons.menu, color: Colors.white)));
+            icon: const Icon(Icons.menu, color: Colors.white)));
   }
 
   Widget _buildContent(BuildContext context) {
     return BlocConsumer<MainBloc, MainState>(listener: (context, state) {
       if (state is ResetSuccess) {
-        this._mainBloc.add(FetchSearchInfo(
-            price: this._configs.price,
-            openAt: this._configs.openAtInSec,
-            sortBy: this._configs.sortBy));
+        _mainBloc.add(FetchSearchInfo(
+            price: _configs.price,
+            openAt: _configs.openAtInSec,
+            sortBy: _configs.sortBy));
       }
     }, builder: (context, state) {
       if (state is Success || state is LoadMoreSuccess) {
-        this._summaryInfos = (state is Success)
+        _summaryInfos = (state is Success)
             ? state.summaryInfos
             : (state as LoadMoreSuccess).summaryInfos;
       }
@@ -106,17 +106,17 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
       if (state is InProgress ||
           state is MainInitial ||
           state is ResetSuccess) {
-        return Center(child: LoadingWidget());
+        return const Center(child: LoadingWidget());
       }
 
-      if (this._summaryInfos.isEmpty) {
-        return EmptyDataWidget();
+      if (_summaryInfos.isEmpty) {
+        return const EmptyDataWidget();
       }
 
       // display restaurant list
       return _isListMode
-          ? RestaurantInfoListWidget(this._summaryInfos, this._configs)
-          : MapWidget(this._summaryInfos);
+          ? RestaurantInfoListWidget(_summaryInfos, _configs)
+          : MapWidget(_summaryInfos);
     });
   }
 
@@ -127,16 +127,16 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
         child: SafeArea(
             child: ListView(padding: EdgeInsets.zero, children: [
       DrawerHeader(
-          decoration: BoxDecoration(color: ColorName.appPrimaryColor),
+          decoration: const BoxDecoration(color: ColorName.appPrimaryColor),
           child: Align(
               alignment: Alignment.bottomLeft,
               child: Text(appLocalizations.main_page_title,
-                  style: TextStyle(
+                  style: const TextStyle(
                       color: Colors.white,
                       fontSize: UIConstants.xxxhFontSize,
                       fontWeight: FontWeight.bold)))),
       ListTile(
-          leading: Icon(Icons.search, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.search, color: ColorName.appPrimaryColor),
           title: Text(appLocalizations.keyword_search),
           onTap: () {
             Navigator.of(context).pop();
@@ -145,7 +145,7 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
             });
           }),
       ListTile(
-          leading: Icon(Icons.filter_list, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.filter_list, color: ColorName.appPrimaryColor),
           title: Text(appLocalizations.filter_rules),
           onTap: () {
             Navigator.of(context).pop();
@@ -154,7 +154,7 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
             });
           }),
       ListTile(
-          leading: Icon(Icons.map, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.map, color: ColorName.appPrimaryColor),
           title: Text(
             _isListMode
                 ? appLocalizations.map_mode
@@ -165,36 +165,36 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
             _runAfterDrawerClosed(() {
               if (!mounted) return;
               setState(() {
-                this._isListMode = !this._isListMode;
+                _isListMode = !_isListMode;
               });
-              this._mainBloc.add(Reset());
+              _mainBloc.add(const Reset());
             });
           }),
       ListTile(
-          leading: Icon(Icons.navigation, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.navigation, color: ColorName.appPrimaryColor),
           title: Text(appLocalizations.map_my_loc_title),
           onTap: () {
             Navigator.of(context).pop();
             _runAfterDrawerClosed(() {
-              this._mainBloc.add(Reset());
+              _mainBloc.add(const Reset());
             });
           }),
       ListTile(
-          leading: Icon(Icons.favorite, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.favorite, color: ColorName.appPrimaryColor),
           title: Text(appLocalizations.favorite_store_add),
           onTap: () {
             Navigator.of(context).pop();
             _runAfterDrawerClosed(() {
-              Navigator.of(this.context).pushNamed(FavorPage.ROUTE_NAME);
+              Navigator.of(this.context).pushNamed(FavorPage.routeName);
             });
           }),
       ListTile(
-          leading: Icon(Icons.settings, color: ColorName.appPrimaryColor),
+          leading: const Icon(Icons.settings, color: ColorName.appPrimaryColor),
           title: Text(appLocalizations.settings_title),
           onTap: () {
             Navigator.of(context).pop();
             _runAfterDrawerClosed(() {
-              Navigator.of(this.context).pushNamed(SettingsPage.ROUTE_NAME);
+              Navigator.of(this.context).pushNamed(SettingsPage.routeName);
             });
           })
     ])));
@@ -218,16 +218,16 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
         msgWidget: PlatformTextField(
           hintText: S.current.keyword_search_hint,
           onChanged: (keyword) {
-            this._filterKeyword = keyword;
+            _filterKeyword = keyword;
           },
         ),
         actions: [
           PlatformTextButton(
               onPressed: () {
-                this._mainBloc.add(FilterListByKeyword(
-                    keyword: this._filterKeyword,
-                    sortByStr: this._configs.sortBy));
-                this._filterKeyword = "";
+                _mainBloc.add(FilterListByKeyword(
+                    keyword: _filterKeyword,
+                    sortByStr: _configs.sortBy));
+                _filterKeyword = '';
                 Navigator.pop(context);
               },
               child: PlatformText(S.current.confirm)),
@@ -241,17 +241,17 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
 
   Future<void> _openFilterPage() async {
     Tuple2<FilterConfigs, dynamic> arguments =
-        Tuple2<FilterConfigs, dynamic>(this._configs, null);
-    Tuple2<FilterConfigs, dynamic>? result = (await Navigator.of(this.context)
-            .pushNamed(FilterPage.ROUTE_NAME, arguments: arguments))
+        Tuple2<FilterConfigs, dynamic>(_configs, null);
+    Tuple2<FilterConfigs, dynamic>? result = (await Navigator.of(context)
+            .pushNamed(FilterPage.routeName, arguments: arguments))
         as Tuple2<FilterConfigs, dynamic>?;
 
     if (result == null || !mounted) {
       return;
     }
 
-    this._configs = result.item1;
-    this._mainBloc.add(Reset());
+    _configs = result.item1;
+    _mainBloc.add(const Reset());
   }
 
   void _openDrawer() {
@@ -268,19 +268,19 @@ class MainPageState extends State<MainPage> implements AppOpenADEvent {
       if (!mounted) return;
       // Waiting building is finish and run.
       final args = ModalRoute.of(context)?.settings.arguments
-          as Tuple2<YelpRestaurantSummaryInfo, dynamic>?;
-      YelpRestaurantSummaryInfo? summaryInfoFromNotification = args?.item1;
+          as Tuple2<RestaurantEntity, dynamic>?;
+      RestaurantEntity? summaryInfoFromNotification = args?.item1;
 
       if (summaryInfoFromNotification == null) {
         return;
       }
 
-      Tuple2 arguments = Tuple2<YelpRestaurantSummaryInfo, dynamic>(
+      Tuple2 arguments = Tuple2<RestaurantEntity, dynamic>(
           summaryInfoFromNotification, null);
       // Avoid duplicate push, use pushNamedAndRemoveUntil instead of push
       Navigator.of(context).pushNamedAndRemoveUntil(
-          RestaurantDetailPage.ROUTE_NAME,
-          ModalRoute.withName(MainPage.ROUTE_NAME),
+          RestaurantDetailPage.routeName,
+          ModalRoute.withName(MainPage.routeName),
           arguments: arguments);
     });
   }
