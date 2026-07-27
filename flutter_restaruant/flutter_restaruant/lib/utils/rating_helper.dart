cd30b553b5859a9435a2d4f3739d7f90c4cf59c5
Yianday <yianday0330h@gmail.com>
2026-07-27 22:50:34 +0800

    refactor(utils): add RatingHelper and rename UIConstants to camelCase
    Extract the star-rating image lookup, previously duplicated as a
    getRatingImage() method on several Yelp models, into a shared
    RatingHelper. Rename the UIConstants fields from SCREAMING_CASE to
    camelCase and drop InterstitialADCountDown, now superseded by
    AdCounterManager.
    
    Kept as one commit because RatingHelper reads the renamed
    UIConstants.ratingImage* fields and would not compile without them.

---
+++ flutter_restaruant/flutter_restaruant/lib/utils/rating_helper.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/widgets.dart';
+import 'ui_constants.dart';
+
+class RatingHelper {
+  static final Map<String, Image> _ratingImgMap = <String, Image>{
+    '0.0': Image.asset('images/Star_rating_0_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '0.5': Image.asset('images/Star_rating_0.5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '1.0': Image.asset('images/Star_rating_1_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '1.5': Image.asset('images/Star_rating_1.5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '2.0': Image.asset('images/Star_rating_2_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '2.5': Image.asset('images/Star_rating_2.5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '3.0': Image.asset('images/Star_rating_3_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '3.5': Image.asset('images/Star_rating_3.5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '4.0': Image.asset('images/Star_rating_4_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '4.5': Image.asset('images/Star_rating_4.5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH),
+    '5.0': Image.asset('images/Star_rating_5_of_5.png',
+        width: UIConstants.ratingImageW, height: UIConstants.ratingImageH)
+  };
+
+  static Widget getRatingImage(String? ratingStr) {
+    if (ratingStr == null || ratingStr.isEmpty) {
+      return _ratingImgMap['0.0']!;
+    }
+    int dotIndex = ratingStr.indexOf('.');
+    if (dotIndex == -1) {
+      ratingStr = '$ratingStr.0';
+      dotIndex = ratingStr.indexOf('.');
+    }
+    String ratingBeforeDotStr = ratingStr.substring(0, dotIndex);
+    String ratingAfterDotStr =
+        (ratingStr.substring(dotIndex, ratingStr.length).compareTo('.5') < 1)
+            ? '.0'
+            : '.5';
+
+    String key = '$ratingBeforeDotStr$ratingAfterDotStr';
+    return _ratingImgMap[key] ?? _ratingImgMap['0.0']!;
+  }
+}
