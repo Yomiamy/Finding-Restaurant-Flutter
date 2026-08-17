801e7c096726fd9d8c813b47039a51812478027d
Yianday <yianday0330h@gmail.com>
2026-08-18 01:59:10 +0800

    refactor: revert imageErrorBuilder changes in RestaurantHeadCell

--- flutter_restaruant/flutter_restaruant/lib/component/cell/restaurant_detail/restaurant_head_cell.dart
+++ flutter_restaruant/flutter_restaruant/lib/component/cell/restaurant_detail/restaurant_head_cell.dart
@@ -27,12 +27,8 @@ class RestaurantHeadCell extends StatelessWidget {
       children: <Widget>[
         FadeInImage.assetNetwork(
           placeholder: UIConstants.noImage,
-          imageErrorBuilder: (context, error, trace) => Image.asset(
-            UIConstants.noImage,
-            width: MediaQuery.of(context).size.width,
-            height: RestaurantHeadCell.headImageH.toDouble(),
-            fit: BoxFit.fill,
-          ),
+          imageErrorBuilder: (context, error, trace) =>
+              Image.asset(UIConstants.noImage),
           image: _imageUrl,
           imageCacheHeight: RestaurantHeadCell.headImageH,
           imageCacheWidth: MediaQuery.of(context).size.width.toInt(),
