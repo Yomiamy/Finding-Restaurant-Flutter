185ecce371d0bb8c43fef8fd3cfe2d2cf06fbf1f
Yianday <yianday0330h@gmail.com>
2026-08-18 02:37:56 +0800

    fix: remove accidentally pasted path string in RestaurantItemCell maxLines

--- flutter_restaruant/flutter_restaruant/lib/component/cell/main_page/restaurant_item_cell.dart
+++ flutter_restaruant/flutter_restaruant/lib/component/cell/main_page/restaurant_item_cell.dart
@@ -94,7 +94,7 @@ class RestaurantItemCell extends StatelessWidget {
                 ),
                 Text(
                   _summaryInfo.location?.displayAddressStr ?? '',
-                  maxLines: 2,flutter_restaruant/flutter_restaruant/lib/component/cell/main_page/restaurant_item_skeleton.dart
+                  maxLines: 2,
                   overflow: TextOverflow.ellipsis,
                 ),
                 Text(
